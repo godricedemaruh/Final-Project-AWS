622f12946f11e9cbc85873775476cbb54bd8c2b5
Andreas Ueberschaer <92183713+godricedemaruh@users.noreply.github.com>
2022-02-09 15:12:28 +0100

    fixed some bugs

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
 import dash
 from dash import dcc
 from dash import html
@@ -9,50 +6,23 @@ import pandas as pd
 import regex
 import numpy as np
 import country_converter as coco
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import pycountry
 import os
 
-# file_path = os.path.join(os.getcwd(), "data")
+
+file_path = os.path.join(os.getcwd(), "data")
 
 
-# def load_data(filename, file_path=file_path):
-#     csv_path = os.path.join(file_path, filename)
-#     return pd.read_csv(csv_path)
+def load_data(filename, file_path=file_path):
+    csv_path = os.path.join(file_path, filename)
+    return pd.read_csv(csv_path)
 
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
+server = app.server
+app.config.suppress_callback_exceptions = True
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
 
-# co2_plot_df = pd.read_csv("co2_plot_df.csv")
-
-urlfile = 'https://raw.githubusercontent.com/owid/energy-data/master/owid-energy-data.csv'
-data_en = pd.read_csv(urlfile)
-
-def start_pipeline(data): 
-    return data.copy()
-
-def df_re_creation(data):
-    data.rename(columns={"year":"Year", "country":"Country"}, inplace=True)
-    data_biofuel = data.filter(regex='biofuel_electricity')
-    data_hydro = data.filter(regex='hydro_electricity')
-    data_o_renew = data.filter(regex='other_renewable_electricity')
-    data_renew = data.filter(regex='renewables_electricity')
-    data_solar = data.filter(regex='solar_electricity')
-    data_wind = data.filter(regex='wind_electricity')
-    data_sev_values = data.filter(['iso_code', 'Country', 'Year', 'electricity_generation', 'primary_energy_consumption'])
-    data = data_sev_values.join(data_wind.join(data_biofuel.join(data_solar.join(data_hydro.join(data_o_renew.join(data_renew))))))
-    data = data.query('Year>=2000').query('Country!="World"').reset_index()
-    arr = np.array(np.where(pd.isnull(data.filter(['iso_code', 'Country'])))).tolist()
-    arr_list = arr[0]
-    data = data.drop(axis=0, index=arr_list).reset_index()
-    data = data.drop(columns={'level_0', 'index'})
-    data.fillna(0, inplace=True)
-    data = data.assign(all_renew_electricity = round((data.other_renewable_electricity + data.renewables_electricity),2))
-    return data
 
 def world_map_re(data):
     fig = px.scatter_geo(data, locations="iso_code", color_discrete_sequence=["green"],
@@ -69,50 +39,6 @@ def world_map_re(data):
             'yanchor': 'top'})
     return fig.show()
 
-def co2_plot_df():
-    url = "https://edgar.jrc.ec.europa.eu/report_2021#emissions_table"
-    driver = webdriver.Chrome('/Users/godri/Documents/WBS/Final_Project/reco2-dict/bin')
-    driver.get(url)
-    test_text = driver.find_element(By.CLASS_NAME, "ecl-table__body").find_element(By.CSS_SELECTOR, 'tr:nth-child(3)').text
-    test_list = [td.text for td in driver.find_elements(By.XPATH, "//tbody[@class='ecl-table__body']/tr")]
-    CO2_Mton = []
-    for j in range(2, len(test_list)):
-        for i in range(len(test_list[j].split(' ')[-8:-1])):
-            if ',' in test_list[j].split(' ')[-8:-1][i]:
-                CO2_Mton.append(test_list[j].split(' ')[-8:-1][i].replace(',',''))
-            else: CO2_Mton.append(test_list[j].split(' ')[-8:-1][i])
-    Country = []
-    for j in range(2, len(test_list)):
-        Country.append(regex.findall("^(.+?)(?=\\d).*", test_list[j])[0])
-        Country.append(regex.findall("^(.+?)(?=\\d).*", test_list[j])[0])
-        Country.append(regex.findall("^(.+?)(?=\\d).*", test_list[j])[0])
-        Country.append(regex.findall("^(.+?)(?=\\d).*", test_list[j])[0])
-        Country.append(regex.findall("^(.+?)(?=\\d).*", test_list[j])[0])
-        Country.append(regex.findall("^(.+?)(?=\\d).*", test_list[j])[0])
-        Country.append(regex.findall("^(.+?)(?=\\d).*", test_list[j])[0])
-    for i in range(len(Country)):
-        if Country[i] == 'Congo ':
-            Country[i] = 'COG'
-        elif Country[i] == 'France and Monaco ':
-            Country[i] = 'France'
-        elif Country[i] == 'Israel and Palestine, State of ':
-            Country[i] = 'Israel'
-        elif Country[i] == 'Italy, San Marino and the Holy See ':
-            Country[i] = 'Italy'
-        elif Country[i] == 'Serbia and Montenegro ':
-            Country[i] = 'SRB'
-        elif Country[i] == 'Spain and Andorra ':
-            Country[i] = 'Spain'
-        elif Country[i] == 'Switzerland and Liechtenstein ':
-            Country[i] = 'Switzerland'
-    year_list = [1990, 2000, 2005, 2010, 2015, 2019, 2020]
-    Year = []
-    for j in range(2, len(test_list)):
-        for years in year_list:
-            Year.append(years)
-    co2_df_plotly = pd.DataFrame({'Country_1':Country, 'Year':Year, 'CO2_Mton':CO2_Mton})
-    co2_df_plotly['CO2_Mton'] = pd.to_numeric(co2_df_plotly['CO2_Mton'], downcast="float")
-    return co2_df_plotly
 
 def plotly_fct(data):
     data = data.query('Country_1!="International Aviation "').query('Country_1!="International Shipping "')
@@ -136,20 +62,16 @@ def plotly_fct(data):
             'yanchor': 'top'})
     return fig.show()
 
-co2_df = co2_plot_df()
 
-fig_re = data_en.pipe(start_pipeline).pipe(df_re_creation).pipe(world_map_re)
-fig_co2 = co2_df.pipe(plotly_fct)
+fig_re = world_map_re(world_map_en_df)
+fig_co2 = plotly_fct(co2_df)
 
 app.layout = html.Div(children=[
-    # All elements from the top of the page
     html.Div([
-        html.H1(children='RECO2 Maps'),
-        html.H2(children="A web application to show the global growth of electricity production with Renewable Energies in relation to the growth of CO2-emissions. 'RECO2'")
-        html.H4("by Andreas Ueberschaer"),
+        html.H1(children='Hello Dash'),
 
         html.Div(children='''
-            World Map displaying the growth of electricity production with renewable energies.
+            Dash: A web application framework for Python.
         '''),
 
         dcc.Graph(
@@ -157,11 +79,11 @@ app.layout = html.Div(children=[
             figure=fig_re
         ),  
     ]),
-    # New Div for all elements in the new 'row' of the page
     html.Div([
-        
+        html.H1(children='Hello Dash'),
+
         html.Div(children='''
-            World Map displaying the growth of CO2-emissions.
+            Dash: A web application framework for Python.
         '''),
 
         dcc.Graph(
