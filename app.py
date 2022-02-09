5a0a8afcf0a14a6a8ff668f89f8f6991b87fde44
Andreas Ueberschaer <92183713+godricedemaruh@users.noreply.github.com>
2022-02-09 17:57:37 +0100

    excluded the functions

--- app.py
+++ app.py
@@ -17,57 +17,55 @@ def load_data(filename, file_path=file_path):
     csv_path = os.path.join(file_path, filename)
     return pd.read_csv(csv_path)
 
-world_map_en_df = pd.read_csv('world_map_re.csv')
-co2_df = pd.read_csv("co2_plot_df.csv")
+en_df = load_data('world_map_re.csv')
+co2_df = load_data("co2_plot_df.csv")
 
 
 app = dash.Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 server = app.server
-app.config.suppress_callback_exceptions = True
 
 
 
-def world_map_re(data):
-    fig = px.scatter_geo(data, locations="iso_code", color_discrete_sequence=["green"],
-                     hover_name="Country", size="all_renew_electricity",
+
+fig_re = px.scatter_geo(en_df, locations="iso_code", color_discrete_sequence=["green"],
+                 hover_name="Country", size="all_renew_electricity",
+                 animation_frame="Year",
+                 projection="natural earth", width=1200, height=800, size_max=70,
+                     title='Electricity with renewable energies [TWh]')
+fig_re.update_layout(
+    title={
+        'text': "Electricity with renewable energies [TWh]",
+        'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'})
+fig_re.show()
+
+
+
+co2_df = co2_df.query('Country_1!="International Aviation "').query('Country_1!="International Shipping "')
+name_list = co2_df['Country_1'].tolist()
+Country_list_names = coco.convert(names=name_list, to='name_short')
+co2_df['Country'] = Country_list_names
+co2_df = co2_df.drop(columns={'Country_1'})
+iso_list = coco.convert(names=Country_list_names, to='ISO3')
+co2_df['iso_3'] = iso_list
+
+fig_co2 = px.scatter_geo(co2_df, locations="iso_3", color_discrete_sequence=["red"],
+                     size="CO2_Mton",
                      animation_frame="Year",
-                     projection="natural earth", width=1200, height=800, size_max=70,
-                         title='Electricity with renewable energies [TWh]')
-    fig.update_layout(
-        title={
-            'text': "Electricity with renewable energies [TWh]",
-            'y':0.9,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'})
-    return fig.show()
+                     projection="natural earth", width=1200, height=800, size_max=60,
+                         title='CO2-Emissions [Mton]')
+fig_co2.update_layout(
+    title={
+        'text': "CO2-Emissions [Mton]",
+        'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'})
+fig_co2.show()
 
 
-def plotly_fct(data):
-    data = data.query('Country_1!="International Aviation "').query('Country_1!="International Shipping "')
-    name_list = data['Country_1'].tolist()
-    Country_list_names = coco.convert(names=name_list, to='name_short')
-    data['Country'] = Country_list_names
-    data = data.drop(columns={'Country_1'})
-    iso_list = coco.convert(names=Country_list_names, to='ISO3')
-    data['iso_3'] = iso_list
-    fig = px.scatter_geo(data, locations="iso_3", color_discrete_sequence=["red"],
-                         size="CO2_Mton",
-                         animation_frame="Year",
-                         projection="natural earth", width=1200, height=800, size_max=60,
-                             title='CO2-Emissions [Mton]')
-    fig.update_layout(
-        title={
-            'text': "CO2-Emissions [Mton]",
-            'y':0.9,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'})
-    return fig.show()
-
-
-fig_re = world_map_re(world_map_en_df)
-fig_co2 = plotly_fct(co2_df)
 
 app.layout = html.Div(children=[
     html.Div([
@@ -80,10 +78,11 @@ app.layout = html.Div(children=[
         '''),
 
         dcc.Graph(
-            id='renewable energy',
+            id='fig_re',
             figure=fig_re
         ),  
     ]),
+
     html.Div([
         
         html.Div(children='''
@@ -91,7 +90,7 @@ app.layout = html.Div(children=[
         '''),
         
         dcc.Graph(
-            id='greenhouse gas',
+            id='fig_co2',
             figure=fig_co2
         ),  
     ]),
